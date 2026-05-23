01c6785700e4c0b8b77fd59d64732cac5fcd0ec9
Ruhul Amin <ruhulamin824854@gmail.com>
2026-05-24 00:19:29 +0600

    login api create and token generate complete

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express, { type Application, type Request, type Response } from "express"
 import { pool } from "./db"
+import config from "./config/envdot"
+import jwt from "jsonwebtoken";
 
 const app: Application = express()
 
@@ -42,5 +44,76 @@ app.post("/api/signup", async (req: Request, res: Response) => {
         });
     }
 });
+
+
+// User Login
+app.post("/api/login", async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    try {
+        // find user by email
+        const result = await pool.query(
+            `SELECT * FROM users WHERE email=$1`,
+            [email]
+        );
+
+        // user not found
+        if (result.rows.length === 0) {
+            return res.status(404).json({
+                success: false,
+                message: "User not found"
+            });
+        }
+
+        const user = result.rows[0];
+
+        // password check
+        if (password !== user.password) {
+            return res.status(401).json({
+                success: false,
+                message: "Invalid password"
+            });
+        }
+
+        // create jwt token
+        const token = jwt.sign(
+            {
+                id: user.id,
+                name: user.name,
+                role: user.role
+            },
+            config.jwt_secret as string,
+            {
+                expiresIn: "7d"
+            }
+        );
+
+        // success response
+        res.status(200).json({
+            success: true,
+            message: "Login successful",
+            data: {
+                token,
+                user: {
+                    id: user.id,
+                    name: user.name,
+                    email: user.email,
+                    role: user.role,
+                    created_at: user.created_at,
+                    updated_at: user.updated_at
+                }
+            }
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Something went wrong",
+            error
+        });
+
+    }
+
+});
+
 
 export default app
