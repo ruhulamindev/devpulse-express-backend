30e42e64389ecfcfac6016c052d943939e47a0a6
Ruhul Amin <ruhulamin824854@gmail.com>
2026-05-23 23:41:44 +0600

    issue table create and middleware-modules-route file add

--- src/db/index.ts
+++ src/db/index.ts
@@ -9,6 +9,7 @@ export const pool = new Pool({
 // DB signup user Table Create
 export const initDB = async () => {
     try {
+        // users table
         await pool.query(`CREATE TABLE IF NOT EXISTS users(
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
@@ -20,6 +21,28 @@ export const initDB = async () => {
     created_at TIMESTAMP DEFAULT NOW(),
     updated_at TIMESTAMP DEFAULT NOW()
       )`)
+
+      // issues table
+      await pool.query(`
+        CREATE TABLE IF NOT EXISTS issues(
+            id SERIAL PRIMARY KEY,
+
+            title VARCHAR(150) NOT NULL,
+
+            description TEXT NOT NULL,
+
+            type VARCHAR(30) NOT NULL,
+
+            status VARCHAR(30) DEFAULT 'open',
+
+            reporter_id INTEGER NOT NULL,
+
+            created_at TIMESTAMP DEFAULT NOW(),
+
+            updated_at TIMESTAMP DEFAULT NOW()
+        )
+        `)
+        
         console.log("Database connected successfully!!");
     } catch (error) {
         console.log(error);
